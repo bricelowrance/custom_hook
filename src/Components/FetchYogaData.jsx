711f5ca631cd0ff5d180cf683b4ad522608347fa
bricelowrance <bricelowrance@gmail.com>
2024-07-25 18:59:10 -0400

    updated UseFetch and FetchData

---
+++ src/Components/FetchYogaData.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import UseFetch from './UseFetch';
+
+const FetchYogaData = () => {
+
+    const [data]=UseFetch('https://api.npoint.io/4459a9a10e43812e1152');
+    
+    return (
+        <>
+        </>
+        )
+    }
+
+export default FetchYogaData
